f2a62b6a4fceb9cb1d3c9009034849b48507962e
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-05-22 20:43:35 -0500

    Lab10

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Genero;
+use App\Models\User;
+use App\Models\Artista;
+use App\Models\Cancione;
+use App\Models\Herramienta;
+use App\Models\Estadisticas_cancione;
+use App\Models\Estadisticas_artista;
+use App\Models\Estadisticas_genero;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +23,51 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/generos', function () {
+    return Genero::ALL();
+});
+
+Route::get('/artistas', function () {
+    return Artista::ALL();
+});
+
+Route::get('/canciones', function () {
+    return Cancione::ALL();
+});
+
+Route::get('/users', function () {
+    return User::ALL();
+});
+
+Route::get('/herramientas', function () {
+    return Herramienta::ALL();
+});
+
+Route::get('/estadisticas_canciones', function () {
+    return Estadisticas_cancione::ALL();
+});
+
+Route::get('/estadisticas_artistas', function () {
+    return Estadisticas_artista::ALL();
+});
+
+Route::get('/estadisticas_generos', function () {
+    return Estadisticas_genero::ALL();
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
